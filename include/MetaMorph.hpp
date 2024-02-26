5c7eefef1a03b94b62b5e25708311f15d48a6d95
= <deforesttthompson@gmail.com>
2024-02-26 11:43:59 -0500

    add pragma once

--- include/MetaMorph.hpp
+++ include/MetaMorph.hpp
@@ -1,4 +1,5 @@
-#define DECORATOR(name, ...) template< class T > auto name(T&& function) { auto wrapped = [func = std::forward<T>(function)](auto&&... args)
+#pragma once
+#define DECORATOR(name) template< class T > auto name(T&& function) { auto wrapped = [func = std::forward<T>(function)](auto&&... args)
 #define END_DECORATOR ; return wrapped; }
 #define DECORATE(decorator, name, ...) auto name = decorator([](__VA_ARGS__) -> decltype(auto)
 #define END_DECORATE );
