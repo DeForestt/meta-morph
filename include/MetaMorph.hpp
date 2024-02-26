41cd43afe8919de155032d39ae62c29859a825ea
= <deforesttthompson@gmail.com>
2024-02-26 11:13:22 -0500

    first commit

---
+++ include/MetaMorph.hpp
@@ -0,0 +1,5 @@
+#define DECORATOR(name, ...) template< class T > auto name(T&& function) { auto wrapped = [func = std::forward<T>(function)](auto&&... args)
+#define END_DECORATOR ; return wrapped; }
+#define DECORATE(decorator, name, ...) auto name = decorator([](__VA_ARGS__) -> decltype(auto)
+#define END_DECORATE );
+#define SUBJ() func(std::forward<decltype(args)>(args)...)
